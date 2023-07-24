c5cad619db849125bda22068eda654af9072f9e5
Abubakar <abubakar.usba@gmail.com>
2023-07-23 23:28:39 -0700

    few change

---
+++ main.js
@@ -0,0 +1,15 @@
+var number = 3;
+if(number==3) {
+    console.log("correct")
+}
+else {
+    console.log("incorrrect")
+}
+
+var success = true;
+if(success===true){
+    console.log("true")
+}
+else{
+    console.log("false")
+}
